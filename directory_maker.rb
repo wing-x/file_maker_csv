185be7bee60675901be9db70177e88fc8a6905a7
hiroshi <sisakux@gmail.com>
2017-05-15 04:19:33 +0900

    少し修正

--- directory_maker.rb
+++ directory_maker.rb
@@ -2,8 +2,9 @@ Encoding.default_external = Encoding::UTF_8
 require 'csv'
 require 'fileutils'
 
-dirlist = CSV.readlines("dirlist.csv").flatten #ネストした状態で格納されてる、でも普通の区切りで問題ないのでflatten
+dirlist = CSV.readlines("dirlist_AOJ.csv").flatten #ネストした状態で格納されてる、でも普通の区切りで問題ないのでflatten
 
 dirlist.each do |i|
-  FileUtils.mkdir_p("#{i}") #ダブルコーテションじゃないとiが反応しないので注意
+  FileUtils.mkdir_p(i) #ダブルコーテションじゃないとiが反応しないので注意
 end
+
--- dirmaker_test.rb
+++ dirmaker_test.rb
@@ -2,10 +2,10 @@ Encoding.default_external = Encoding::UTF_8
 require 'csv'
 require 'fileutils'
 
-dirlist = CSV.readlines("dirlist.csv").flatten #ネストした状態で格納されてる、でも普通の区切りで問題ないのでflatten
+dirlist = CSV.readlines("dirlist_AOJ.csv").flatten #ネストした状態で格納されてる、でも普通の区切りで問題ないのでflatten
 
 dirlist.each do |i|
-  FileUtils.mkdir_p("#{i}") #ダブルコーテションじゃないとiが反応しないので注意
+  FileUtils.mkdir_p(i) #ダブルコーテションじゃないとiが反応しないので注意
 end
 
 #挙動確認用
