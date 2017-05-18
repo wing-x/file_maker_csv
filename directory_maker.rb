0abcf97644efca862fc7e42f5bcfc8f4f4889503
hiroshi <sisakux@gmail.com>
2017-05-19 02:33:27 +0900

    tuiki

--- directory_maker.rb
+++ directory_maker.rb
@@ -2,7 +2,7 @@ Encoding.default_external = Encoding::UTF_8
 require 'csv'
 require 'fileutils'
 
-dirlist = CSV.readlines("dirlist_AOJ.csv").flatten #ネストした状態で格納されてる、でも普通の区切りで問題ないのでflatten
+dirlist = CSV.readlines("hackerrank_java3.csv").flatten #ネストした状態で格納されてる、でも普通の区切りで問題ないのでflatten
 
 dirlist.each do |i|
   FileUtils.mkdir_p(i) #ダブルコーテションじゃないとiが反応しないので注意
